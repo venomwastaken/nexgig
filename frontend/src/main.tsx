24550a747b2470fa91a7e47e33f4616707aca976
venomwastaken <109553049+venomwastaken@users.noreply.github.com>
2026-06-27 21:14:45 +0000

    tailwind added and configured

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,18 +2,19 @@ import { ClerkProvider } from "@clerk/react";
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import "./index.css";
 
-const clerk_key = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const clerk_key = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
-if (!clerk_key) throw Error("Clerk Key not found")
+if (!clerk_key) throw Error("Clerk Key not found");
 
-const rootElement = document.getElementById('root')
-if (!rootElement) throw Error("Root element not found")
+const rootElement = document.getElementById("root");
+if (!rootElement) throw Error("Root element not found");
 
 createRoot(rootElement).render(
     <StrictMode>
         <ClerkProvider publishableKey={clerk_key}>
             <App />
         </ClerkProvider>
-    </StrictMode>
-)
+    </StrictMode>,
+);
